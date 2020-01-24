1ca18b5049f432cb77821ce426f8fa606f3591f6
jcrn <clemons.jason@gmail.com>
2020-01-24 10:22:05 -0800

    MVP complete

--- adv.py
+++ adv.py
@@ -5,6 +5,7 @@ from world import World
 import random
 from ast import literal_eval
 
+
 # Load world
 world = World()
 
@@ -29,6 +30,66 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+def traversal(player, world, graph):
+    
+    # Create a path list (p)
+    p = []    
+    
+    # Create a room stack (s) and add the starting room
+    s = []
+    s.append(player.current_room.id)
+    
+    # Create a set for visited rooms (v) - set is both ordered and unique values
+    v = set()    
+        
+    # While all of the rooms (len(v)) have not been visited
+    while len(v) != len(world.rooms):
+        # Get the current room from the stack (s)
+        cur = s[-1]
+        # Add the current room to visited (v)
+        v.add(cur)
+        
+        # Handle the connecting rooms
+        # Create a queue for a BFT (q)
+        q = []        
+        
+        # Get the connecting rooms (cr) from the graph
+        # i.e. {'n': 361, 'e': 321, 'w': 386}
+        cr = graph[cur][1] 
+        
+        # Check for connected, unvisited rooms:
+        # For any connected rooms 
+        for x, y in cr.items():
+            # if room (y) is not in visited (v) add it to the queue (q)
+            if y not in v:
+                q.append(y)
+        
+        # Get the next room:
+        # If there are rooms in the queue (q), get the next room from the queue (q)       
+        if len(q):
+            # Dequeue the next room (q)
+            nxt = q[0]
+            # Add it to the stack (s)
+            s.append(nxt)
+        else:
+            # Queue (q) is empty pop the current room from the stack (s) 
+            s.pop()
+            # Get next room (nxt) from the stack (s)
+            nxt = s[-1]
+
+        # Add the cardinal directions to the path (p)
+        # For any connected rooms 
+        for x, y in cr.items():
+            # If room is next room (nxt) 
+            if y == nxt:
+                # Add the cardinal direction to the path (p)
+                p.append(x)
+    
+    # Return the path (p) of cardinal directions   
+    return p
+
+
+traversal_path = traversal(player, world, room_graph)    
 
 
 # TRAVERSAL TEST
